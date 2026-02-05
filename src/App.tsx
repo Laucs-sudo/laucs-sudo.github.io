03ac9546acdb24d6a9cc65e8f7ae7b336482a0a1
Laucs-sudo <donaldsonruss590@gmail.com>
2026-02-05 07:10:07 -0500

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-export function App() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center bg-black">
-      <h1 className="text-white text-4xl md:text-6xl font-bold tracking-widest uppercase">
-        Coming Soon
-      </h1>
-    </div>
-  );
-}
